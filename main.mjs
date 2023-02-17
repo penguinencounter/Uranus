b8a41448e934c1a0a44a9d84102acc77d4f5edb9
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2023-02-16 16:58:06 -0800

    Dynamic content

--- main.mjs
+++ main.mjs
@@ -1,7 +1,7 @@
 window.addEventListener("load", () => {
-    document.getElementById("tab").addEventListener("click", ev => {
-        ev.preventDefault();
-        ev.stopPropagation();
+    const scheduleList = document.getElementById("all-schedules");
+
+    const toggleTab = () => {
         const configureTab = document.getElementById("configure");
         const tab = document.getElementById("tab");
         if (configureTab.classList.contains("collapsed")) {
@@ -13,5 +13,77 @@ window.addEventListener("load", () => {
             configureTab.classList.remove("opened");
             tab.innerHTML = "Configure";
         }
+    }
+
+    document.getElementById("tab").addEventListener("click", ev => {
+        ev.preventDefault();
+        ev.stopPropagation();
+        toggleTab();
     });
+
+    function refreshScheduleList() {
+        let childNo = 1;
+        for (let child of scheduleList.children) {
+            child.querySelector(".schedule-title > span").innerHTML = `Schedule #${childNo++}`;
+        }
+    }
+
+    function newSchedule() {
+        const schedule = document.createElement("div");
+        schedule.classList.add("schedule");
+        const scheduleTitle = document.createElement("div");
+        scheduleTitle.classList.add("schedule-title");
+        const scheduleTitleSpan = document.createElement("span");
+        scheduleTitleSpan.innerHTML = `Schedule #${scheduleList.children.length + 1}`;
+        const picker = document.createElement("div");
+        picker.classList.add("dotw-picker");
+        const daysOfTheWeek = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
+        for (let i = 0; i < 7; i++) {
+            const day = daysOfTheWeek[i];
+            const button = document.createElement("button");
+            button.classList.add("day-of-the-week");
+            button.innerHTML = day;
+            button.addEventListener("click", ev => {
+                ev.preventDefault();
+                ev.stopPropagation();
+                if (ev.target.classList.contains("selected")) {
+                    ev.target.classList.remove("selected");
+                } else {
+                    ev.target.classList.add("selected");
+                }
+            })
+            button.dataset.day = i.toString();
+            picker.appendChild(button);
+        }
+        const deleteButton = document.createElement("button");
+        deleteButton.classList.add("delete-schedule");
+        deleteButton.innerHTML = "Delete";
+        deleteButton.addEventListener("click", ev => {
+            ev.preventDefault();
+            ev.stopPropagation();
+            schedule.remove();
+            refreshScheduleList();
+        });
+
+        scheduleTitle.appendChild(scheduleTitleSpan);
+        scheduleTitle.appendChild(picker);
+        scheduleTitle.appendChild(deleteButton);
+
+        schedule.appendChild(scheduleTitle);
+        scheduleList.appendChild(schedule);
+        refreshScheduleList();
+        return schedule;
+    }
+
+    document.querySelector(".new-schedule").addEventListener("click", newSchedule);
+
+    document.body.addEventListener("keydown", ev => {
+        console.info("Key pressed: " + ev.key)
+        if (ev.key === "Tab") {
+            ev.preventDefault();
+            ev.stopPropagation();
+            if (ev.repeat) return;
+            toggleTab();
+        }
+    })
 });
